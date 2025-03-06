3f4319b154c0a6173b7a351b8a9ad890397bc410
Mia Strinic <mstrin01@fesb.hr>
2025-03-06 14:44:29 +0100

    favorites

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,24 @@ import Favorites from './pages/Favorites';
 import Home from './pages/Home';
 import {Routes, Route} from "react-router-dom";
 import NavBar from './components/NavBar';
+import { MovieProvider } from './contexts/MovieContext';
 
 
 function App() {
 
 
   return (
-    <div>
-      <NavBar />
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/favorites" element={<Favorites />}/>
 
-      </Routes>
-    </main>
-    </div>
+    <MovieProvider>
+        <NavBar />
+        <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+
+        </Routes>
+      </main>
+    </MovieProvider>
 
     
   )
